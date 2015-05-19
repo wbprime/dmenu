857491c912720c5f011155a32a7960e0407be9ab
Elvis Wang <mail@wbprime.me>
2015-05-19 15:46:30 +0800

    Change font to monospace which is more common; change list direction to horizontal

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 /* Default settings; can be overrided by command line. */
 
 static Bool topbar = True;                  /* -b  option; if False, dmenu appears at bottom */
-static const char *font = "Inconsolata-13:lcdfilter=lcddefault:hintstyle=hintfull:hinting=full:antialias=true:autohint=false";             /* -fn option; default X11 font or font set      */
+static const char *font = "Monospace:size=13:lcdfilter=lcddefault:hintstyle=hintfull:hinting=full:antialias=true:autohint=false";             /* -fn option; default X11 font or font set      */
 static const char *prompt = "[search]";           /* -p  option; prompt to the elft of input field */
 static const char *normbgcolor = "#222222"; /* -nb option; normal background                 */
 static const char *normfgcolor = "#bbbbbb"; /* -nf option; normal foreground                 */
@@ -13,5 +13,5 @@ static const char *selfgcolor  = "#eeeeee"; /* -sf option; selected foreground  
 static const char *outbgcolor  = "#00ffff";
 static const char *outfgcolor  = "#000000";
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines = 9;
+static unsigned int lines = 0;
 
